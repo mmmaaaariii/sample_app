a983028a9aea280ab80aee5ca8b2cb0f65abaaf7
mmmaaaariii <166019982+mmmaaaariii@users.noreply.github.com>
2024-05-13 13:00:02 +0000

    [FIN]11-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,14 +4,23 @@ class ListsController < ApplicationController
     @list = List.new
   end
   
-  def create
-    #インスタンス作成：データを受け取り新規登録
-    list = List.new(list_params)
-    #メソッド実行：データをデータベースに保尊するためのsaveメソッド
-    list.save
-    # リダイレクト：トップ画面
-    redirect_to list_path(list.id)
-  end
+  
+   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+   end  
+  # def create
+  #   #インスタンス作成：データを受け取り新規登録
+  #   list = List.new(list_params)
+  #   #メソッド実行：データをデータベースに保尊するためのsaveメソッド
+  #   list.save
+  #   # リダイレクト：トップ画面
+  #   redirect_to list_path(list.id)
+  # end
 
   def index
     @lists = List.all
